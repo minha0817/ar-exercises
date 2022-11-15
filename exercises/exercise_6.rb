403e5ccd6040c88cc74fc9dd01b43cbd2cb5fc4c
minha0817 <minhakk24@gmail.com>
2022-11-14 21:20:44 -0800

    add One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Add some data into employees. Here's an example of one (note how it differs from how you create stores): 
+#`@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Minha", last_name: "Kim", hourly_rate: 120)
+@store1.employees.create(first_name: "Yooni", last_name: "Yoon", hourly_rate: 120)
+@store1.employees.create(first_name: "Apple", last_name: "Park", hourly_rate: 100)
+
+
+@store2.employees.create(first_name: "Chris", last_name: "Son", hourly_rate: 140)
+@store2.employees.create(first_name: "Angela", last_name: "Lee", hourly_rate: 70)
+@store2.employees.create(first_name: "Sam", last_name: "Lee", hourly_rate: 80)
+@store2.employees.create(first_name: "Kevin", last_name: "Kim", hourly_rate: 50)
+
