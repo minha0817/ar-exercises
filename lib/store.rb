cdf89ebeffafb4ab5351451637459e83cdfc32be
minha0817 <minhakk24@gmail.com>
2022-11-14 22:43:42 -0800

    add validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
+    has_many :employees
+
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_integer: true}
+    validates :annual_revenue, numericality: { greater_than_or_equal_to: 0}
+    validate :must_carry_apparel_of_at_least_one_gender
+
+    def must_carry_apparel_of_at_least_one_gender
+        if !mens_apparel && !womens_apparel
+            errors.add(name, "must carry at least one of the men's or women's apparel")
+        end
+    end
 
 end
