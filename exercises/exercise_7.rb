5161c506fe3e9fc913ac1d8a1ece83bdea5bba7d
minha0817 <minhakk24@gmail.com>
2022-11-14 22:43:19 -0800

    add validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Ask the user for a store name (store it in a variable)
+
+puts "Enter a store name :"
+
+
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+
+store_name = $stdin.gets.chomp
+
+
+# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+store = Store.create(name: store_name)
+puts store.valid?
+puts store.validate
+error_container = store.errors.full_messages
+puts error_container
